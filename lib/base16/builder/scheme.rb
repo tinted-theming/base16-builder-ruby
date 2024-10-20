aa381994520596df49d2cd12e3fcd37945661697
Hartley McGuire <skipkayhil@gmail.com>
2024-10-19 22:15:08 -0400

    Refactor Scheme#new to not require Psych
    Similarly to the refactor to Template#new, this commit makes it easier
    to instantiate Schemes without yaml files which will make it easier to
    test.

--- lib/base16/builder/scheme.rb
+++ lib/base16/builder/scheme.rb
@@ -10,26 +10,30 @@ module Base16
 
       class << self
         def all
-          @all ||= Dir["schemes/*/*.yaml"].map { |file_path| Scheme.new(file_path: file_path) }
+          @all ||= Dir["schemes/*/*.yaml"].map { |file_path| load(file_path) }
+        end
+
+        def load(path)
+          filename = File.basename(path, ".yaml")
+
+          new(filename.slugify, Psych.safe_load_file(path))
         end
       end
 
-      def initialize(file_path:)
-        yaml = Psych.safe_load_file(file_path)
-        filename = File.basename(file_path, ".yaml")
-
+      def initialize(slug, yaml)
         @author = yaml["author"]
         @name = yaml["scheme"]
-        @slug = filename.slugify
+        @slug = slug
         @bases = {}
 
-        bases = %w[00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F]
-
-        bases.each do |b|
-          key = "base#{b}"
-          @bases[key] = yaml[key]
+        BASES.each do |b|
+          @bases[b] = yaml[b]
         end
       end
+
+      private
+
+      BASES = %w[00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F].map { |suffix| "base#{suffix}" }.freeze
     end
   end
 end
