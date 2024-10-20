cea0bf2168bc2d46b85112970e4805dcfe581fd3
Hartley McGuire <skipkayhil@gmail.com>
2024-10-19 22:40:03 -0400

    Refactor context creation onto Scheme
    Since context creation doesn't involve Templates at all, and
    additionally can now be cached per Scheme so that its only created once
    for each Scheme and not M (Templates) times per Scheme.
    
    This brings total runtime from ~5s to ~2s.

--- lib/base16/builder/template.rb
+++ lib/base16/builder/template.rb
@@ -1,5 +1,4 @@
 require "mustache"
-require "chroma"
 
 module Base16
   module Builder
@@ -33,9 +32,7 @@ module Base16
 
       def render(scheme:)
         @mustaches.each do |mustache, template_file_config|
-          template_data = build_template_data(scheme: scheme)
-
-          rendered_template = mustache.render(template_data)
+          rendered_template = mustache.render(scheme.to_context)
 
           rendered_filename = "base16-#{scheme.slug}#{template_file_config["extension"]}"
           rendered_dir = "out/#{template_file_config["output"]}"
@@ -45,37 +42,6 @@ module Base16
           puts "building #{rendered_dir}/#{rendered_filename}"
           File.write("#{rendered_dir}/#{rendered_filename}", rendered_template)
         end
-      end
-
-      private
-
-      def build_template_data(scheme:)
-        data = {
-          "scheme-name" => scheme.name,
-          "scheme-author" => scheme.author,
-          "scheme-slug" => scheme.slug
-        }
-
-        scheme.bases.each do |base_key, base_color|
-          data["#{base_key}-hex"] = base_color
-
-          data["#{base_key}-hex-r"] = base_color[0, 2]
-          data["#{base_key}-hex-g"] = base_color[2, 2]
-          data["#{base_key}-hex-b"] = base_color[4, 2]
-
-          # Turn hex color into an array of [r, g, b]
-          rgb = base_color.paint.to_rgb.scan(/\d+/)
-
-          data["#{base_key}-rgb-r"] = rgb[0]
-          data["#{base_key}-rgb-g"] = rgb[1]
-          data["#{base_key}-rgb-b"] = rgb[2]
-
-          data["#{base_key}-dec-r"] = rgb[0].to_i / 255.0
-          data["#{base_key}-dec-g"] = rgb[1].to_i / 255.0
-          data["#{base_key}-dec-b"] = rgb[2].to_i / 255.0
-        end
-
-        data
       end
     end
   end
